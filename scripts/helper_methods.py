a9081cbbdc5ab1a3c726841a3bf77be01e23d4d1
0skarian <0skarianftw@gmail.com>
2022-12-12 03:42:08 +0100

    Updated fonts and start screen

---
+++ scripts/helper_methods.py
@@ -0,0 +1,14 @@
+#------------------------------------------------------------------------------------------
+#   Ripple / helper_methods
+#------------------------------------------------------------------------------------------
+
+def lerp(a: float, b: float, t: float) -> float:
+    """Linear interpolate on the scale given by a to b, using t as the point on that scale.
+    Examples
+    --------
+        50 == lerp(0, 100, 0.5)
+        4.2 == lerp(1, 5, 0.8)
+    """
+    return (1 - t) * a + t * b
+
+
